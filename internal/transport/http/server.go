5345e59faba2ec3697ad2e0c23a210d0e73d8b3f
wesleyburlani <wesleyburlani@gmail.com>
2023-10-08 23:20:34 -0300

    add service name on otelchi middleware

--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -6,13 +6,14 @@ import (
 	"github.com/defval/di"
 	"github.com/go-chi/chi/v5"
 	"github.com/riandyrn/otelchi"
+	"github.com/wesleyburlani/go-rest/internal/config"
 	pkg_http_controllers "github.com/wesleyburlani/go-rest/pkg/http/controllers"
 	pkg_http_middlewares "github.com/wesleyburlani/go-rest/pkg/http/middlewares"
 )
 
 func CreateApp(c *di.Container) http.Handler {
 	r := chi.NewRouter()
-	r.Use(otelchi.Middleware("my-server", otelchi.WithChiRoutes(r)))
+	c.Invoke(func(c *config.Config) { r.Use(otelchi.Middleware(c.ServiceName, otelchi.WithChiRoutes(r))) })
 	c.Invoke(func(m *pkg_http_middlewares.Logger) { r.Use(m.Handle) })
 	r.Group(func(r chi.Router) {
 		c.Invoke(func(c *pkg_http_controllers.Health) { r.Mount("/health", c.Router()) })
